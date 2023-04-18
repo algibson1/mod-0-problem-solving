a9c724480e3dfb17e56996c948455f6a84d2a6b5
Artemy Gibson <artemygibson@gmail.com>
2023-04-17 19:35:54 -0700

    Finish challenge 2

--- challenge2.rb
+++ challenge2.rb
@@ -1,1 +1,8 @@
 # Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+array = ["Orderly", "Finally", "Hello", "FSEhFDk", "fsKFFKJhjl", "sfGHJLKdsjfhl"]
+
+
+array.each do |word|
+    p word.downcase
+end
